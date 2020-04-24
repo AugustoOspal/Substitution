90e45f93b5d07501052a9d54105d53441c2a3020
Augusto Ospal <49209526+AugustoOspal@users.noreply.github.com>
2020-04-23 22:37:24 -0300

    Add files via upload

--- substitution.c
+++ substitution.c
@@ -15,109 +15,96 @@ int main(int argc, string argv[])
     if (argc != 2)
     {
         printf("Usage: ./substitution key\n");
-
         return 1;
     }
 
-    // save time and memory if argc != 2
-    else
+
+    int lenght_argv = strlen(argv[1]);
+
+    // error 1
+    if (lenght_argv != 26)
     {
-        int lenght_argv = strlen(argv[1]);
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
 
-        // error 1
-        if (lenght_argv != 26)
+
+
+    // error 2
+    for (int i = 0; i < lenght_argv; i++)
+    {
+        if (! isalpha(argv[1][i]))
         {
-            printf("Key must contain 26 characters.\n");
-
+            printf("Key must contain alphabetic characters.\n");
             return 1;
         }
+    }
 
-        else
+    char key_lower[lenght_argv];
+
+    for (int i = 0; i < lenght_argv; i++)
+    {
+        key_lower[i] = tolower(argv[1][i]);
+    }
+
+    // error 3
+    for (int i = 0; i < lenght_argv; i++)
+    {
+        for (int j = i + 1; j < lenght_argv; j++)
         {
-
-            // error 2
-            for (int i = 0; i < lenght_argv; i++)
+            if (key_lower[i] == key_lower[j])
             {
-                if (! isalpha(argv[1][i]))
-                {
-                    printf("Key must contain alphabetic characters.\n");
-
-                    i = lenght_argv;
-
-                    return 1;
-                }
+                printf("Key must not contain repeated characters.\n");
+                return 1;
             }
-
-            char key_lower[lenght_argv];
-
-            for (int i = 0; i < lenght_argv; i++)
-            {
-                key_lower[i] = tolower(argv[1][i]);
-            }
-
-            // error 3
-            for (int i = 0; i < lenght_argv; i++)
-            {
-                for (int j = i + 1; j < lenght_argv; j++)
-                {
-                    if (key_lower[i] == key_lower[j])
-                    {
-                        printf("Key must not contain repeated characters.\n");
-
-                        i = lenght_argv;
-                        j = lenght_argv;
-
-                        return 1;
-                    }
-                }
-            }
-
-            // user input
-            string plaintext = get_string("plaintext:  ");
-
-            int lenght_plaintext = strlen(plaintext);
-            char plaintext_lower[lenght_plaintext];
-
-            for (int i = 0; i < lenght_plaintext; i++)
-            {
-                plaintext_lower[i] = tolower(plaintext[i]);
-            }
-
-            char ciphertext[lenght_plaintext];
-
-            for (int i = 0; i < lenght_plaintext; i++)
-            {
-                if (! isalpha(plaintext[i]))
-                {
-                    ciphertext[i] = plaintext[i];
-                }
-
-                for (int j = 0; j < strlen(alphabet); j++)
-                {
-                    if (plaintext_lower[i] == alphabet[j])
-                    {
-                        if (isupper(plaintext[i]))
-                        {
-                            ciphertext[i] = toupper(key_lower[j]);
-                        }
-
-                        else
-                        {
-                            ciphertext[i] = key_lower[j];
-                        }
-                    }
-                }
-            }
-            
-            // output
-            printf("ciphertext: ");
-
-            for (int i = 0; i < lenght_plaintext; i++)
-            {
-                printf("%c", ciphertext[i]);
-            }
-
-            printf("\n");
         }
     }
+
+    // user input
+    string plaintext = get_string("plaintext:  ");
+
+    int lenght_plaintext = strlen(plaintext);
+    char plaintext_lower[lenght_plaintext];
+
+    for (int i = 0; i < lenght_plaintext; i++)
+    {
+        plaintext_lower[i] = tolower(plaintext[i]);
+    }
+
+    char ciphertext[lenght_plaintext];
+
+    for (int i = 0; i < lenght_plaintext; i++)
+    {
+        if (! isalpha(plaintext[i]))
+        {
+            ciphertext[i] = plaintext[i];
+        }
+
+        for (int j = 0; j < strlen(alphabet); j++)
+        {
+            if (plaintext_lower[i] == alphabet[j])
+            {
+                if (isupper(plaintext[i]))
+                {
+                    ciphertext[i] = toupper(key_lower[j]);
+                }
+
+                else
+                {
+                    ciphertext[i] = key_lower[j];
+                }
+            }
+        }
+    }
+
+    // output
+    printf("ciphertext: ");
+
+    for (int i = 0; i < lenght_plaintext; i++)
+    {
+        printf("%c", ciphertext[i]);
+    }
+
+    printf("\n");
+    return 0;
 }
